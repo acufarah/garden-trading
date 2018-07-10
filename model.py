8269e3a5906af9defc14a4e1cb9b605d0a314917
vagrant <vagrant@vagrant.vm>
2018-07-10 00:51:14 +0000

    Got DB working

--- model.py
+++ model.py
@@ -18,15 +18,15 @@ class User(db.Model):
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(64), nullable=False)
-    fname = db.Column(db.String(55) nullable=False)
-    lname = db.Column(db.String(55) nullable=False)
+    fname = db.Column(db.String(55), nullable=False)
+    lname = db.Column(db.String(55), nullable=False)
     address = db.Column(db.String(120), nullable=False)
     city = db.Column(db.String(100), nullable=False)
     state = db.Column(db.String(2), nullable=False)
     zipcode = db.Column(db.String(15), nullable=False)
     #profile_pic = pass
     about_me = db.Column(db.Text, nullable=False)
-    about_garden = db.Colum(db.Text, nullable=False)
+    about_garden = db.Column(db.Text, nullable=False)
 
 def __repr__(self):
     """Provide helpful representation when printed."""
@@ -38,44 +38,28 @@ class Produce(db.Model):
 	__tablename__ = "produce"
 
 	prod_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'))
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 	#prod_pic = pass
 	prod_name = db.Column(db.String(120), nullable=False)
 	prod_type = db.Column(db.Integer, nullable=True)
 	describe = db.Column(db.Text, nullable=False)
 	avail_date = db.Column(db.Date, nullable=False)
 
+	user = db.relationship("User", backref=db.backref("produce", order_by=prod_id))
+
 def __repr__(self):
     """Provide helpful representation when printed."""
     return f"<Produce prod_id={self.prod_id} user_id={self.user_id} prod_name={self.prod_name} >"
 
-class Message(db.Model):
+"""class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Message {}>'.format(self.body)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return '<Message {}>'.format(self.body)"""
 
 
 
@@ -105,4 +89,6 @@ if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    init_app()
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
--- server.py
+++ server.py
@@ -13,16 +13,76 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
-# Normally, if you use an undefined variable in Jinja2, it fails
-# silently. This is horrible. Fix this so that, instead, it raises an
-# error.
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
     """Homepage."""
     
-    return render_template("homepage.html")
+    return render_template("home.html")
+
+@app.route('/sign_up', methods=["GET"])
+def sign_up():
+	"""Sign up page."""
+
+	return render_template("sign_up.html")
+
+@app.route('/sign_up', methods=["POST"])
+def sign_up_process():
+	"""Sign up process."""
+
+	fname = request.form.get('fname')
+	lname = request.form.get('lname')
+	address = request.form.get('address')
+	city = request.form.get('city')
+	state = request.form.get('state')
+	zipcode = request.form.get('zipcode')
+	username = request.form.get('username')
+	email = request.form.get('email')
+	password = request.form.get('password')
+
+	if User.query.filter_by(username='username').first():
+		flash("User with this username already exists.")
+	elif User.query.filter_by(email='email').first():
+		flash("User with this email already exists.")
+		return redirect('/')
+	else:
+		new_user = User(username=username, email=email, password=password, fname=fname, lname=lname,
+						address=address, city=city, state=state, zipcode=zipcode)
+		db.session.add(new_user)
+		db.session.commit()
+		flash("User added to database. Please log in.")
+		return redirect('/login')
+
+@app.route('/login', methods=["POST"])
+def process_login_form():
+	email = request.form.get('email')
+	password = request.form.get('password')
+
+
+	user = User.query.filter_by(email=email, password=password).first()
+	if user:
+		flash("Login successful.")
+		session['user_id'] = user.user_id
+		return redirect('/users_profile/{}'.format(user.user_id))
+	else:
+		flash("Login unsuccessful.")
+		return redirect('/login')
+
+@app.route('/login')
+def show_login_form():
+	return render_template("login.html")
+
+
+@app.route('/users/<int:uid>')
+def show_user_info(uid):
+
+	print("User UID is: {}".format(uid))
+
+	user = User.query.get(uid)
+
+	return render_template('user_profile.html', user=user, username=user.username, email=user.email, password=user.password, fname=user.fname, lname=user.lname,
+						address=user.address, city=user.city, state=user.state, zipcode=user.zipcode)
 
 
 
